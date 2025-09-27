dc351cd967a6da62cc9d54832b1a0d2c86749038
Christopher Alejandro Maldonado Chavez <christopher.cop787@gmail.com>
2025-09-27 23:24:06 +0000

    Dale un estilo brutalism

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        headline: ['Inter', 'sans-serif'],
+        body: ['"IBM Plex Mono"', 'monospace'],
+        headline: ['"IBM Plex Mono"', 'monospace'],
         code: ['monospace'],
       },
       colors: {
